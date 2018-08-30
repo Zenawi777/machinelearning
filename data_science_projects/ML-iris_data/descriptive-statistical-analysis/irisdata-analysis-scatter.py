8e61919240bdc3adc067af4189279b890ba93345
Zenawi777 <40924568+Zenawi777@users.noreply.github.com>
2018-08-30 17:49:37 +0100

    setting color for the scatter plot

--- data_science_projects/ML-iris_data/descriptive-statistical-analysis/irisdata-analysis-scatter.py
+++ data_science_projects/ML-iris_data/descriptive-statistical-analysis/irisdata-analysis-scatter.py
@@ -14,17 +14,18 @@ df = pd.read_csv('iris-data-copy.txt',header=0,sep=',',usecols=range(4))
 
 # Making a grid of a figure window
 grid_size=(1,3)
+colors=np.random.rand(len(df))
 
 # Scatter plot of the first column against the second 
 _ = plt.subplot2grid(grid_size,(0,0))
-scatter_plot=plt.scatter(df['5.1'],df['3.5'])
+scatter_plot=plt.scatter(df['5.1'],df['3.5'],c=colors)
 
 # Scatter plot of the first with respect to third column
 _ = plt.subplot2grid(grid_size,(0,1))
-scatter_plot=plt.scatter(df['5.1'],df['1.4'])
+scatter_plot=plt.scatter(df['5.1'],df['1.4'],c=colors)
 
 # Scatter plot of the first with respect to fourth column
 _ = plt.subplot2grid(grid_size,(0,2))
-scatter_plot=plt.scatter(df['5.1'],df['0.2'])
+scatter_plot=plt.scatter(df['5.1'],df['0.2'],c=colors)
 
 plt.show() # Displaying the scatter plot
