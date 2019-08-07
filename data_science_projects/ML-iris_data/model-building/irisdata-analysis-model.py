6d5cb6345c4ab5f16c5cfe2030a476c3f1e31280
Zenawi777 <40924568+Zenawi777@users.noreply.github.com>
2019-08-07 21:20:04 +0100

    some changes

--- data_science_projects/ML-iris_data/model-building/irisdata-analysis-model.py
+++ data_science_projects/ML-iris_data/model-building/irisdata-analysis-model.py
@@ -3,12 +3,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from matplotlib import colors
+
 
 
 # Import data from a file
 df = pd.read_csv('iris-data-copy.txt',header=0,sep=',',usecols=range(4))
 
 
+cmap = colors.LinearSegmentedColormap(
+    'red_blue_classes',
+    {'red': [(0, 1, 1), (1, 0.7, 0.7)],
+     'green': [(0, 0.7, 0.7), (1, 0.7, 0.7)],
+     'blue': [(0, 0.7, 0.7), (1, 1, 1)]})
+plt.cm.register_cmap(cmap=cmap)
 
+
+
+
+
+
+
+for i, (X, y) in enumerate([dataset_fixed_cov(), dataset_cov()]):
+    # Linear Discriminant Analysis
+    lda = LinearDiscriminantAnalysis(solver="svd", store_covariance=True)
+    y_pred = lda.fit(X, y).predict(X)
+    splot = plot_data(lda, X, y, y_pred, fig_index=2 * i + 1)
+    plot_lda_cov(lda, splot)
+    plt.axis('tight')
+
+    
 
 
